d98baa81942b34273c6b4db45c25f0a826795930
Alimbo-zzz <alimbo333@gmail.com>
2022-01-07 19:35:00 +0300

    save img

--- Front-end/js/main.js
+++ Front-end/js/main.js
@@ -5,7 +5,7 @@ var baseURL = "http://149.28.153.46:10101/v1";
 var routes = {
   home: '/',
   about: '/about',
-  create: '/create',
+  create: '/create.html',
   deposit: '/deposit',
   myPhotos: '/myPhotos',
   payinfo: '/payinfo'
@@ -64,6 +64,146 @@ function sendRequest(method, url) {
     };
 
     xhr.send(JSON.stringify(body));
+  });
+};"use strict";
+
+function GET_images() {
+  var url = "".concat(baseURL, "/images/list");
+  sendRequest('GET', url, null, token).then(function (res) {
+    addImagesToCollection(res);
+    modal_image();
+  })["catch"](function (err) {
+    return console.log(err);
+  });
+}
+
+var myPhotos = document.querySelector('.s_my-photos');
+if (myPhotos) GET_images();;"use strict";
+
+function GET_imgStatus(image_id) {
+  var url = "".concat(baseURL, "/images/check?image_id=").concat(image_id);
+  var check_image_status = setInterval(function () {
+    sendRequest('GET', url, null, token).then(function (res) {
+      if (res.state != 0) {
+        if (myPhotos) GET_images();
+        clearInterval(check_image_status);
+      }
+    })["catch"](function (err) {
+      return console.log(err);
+    });
+  }, 10000);
+};"use strict";
+
+function GET_pay(cash) {
+  var page_back = "".concat(window.location.origin).concat(routes.payinfo);
+  var url = "".concat(baseURL, "/payments/create?payment_type=1&payment_amount=").concat(cash, "&successfull=").concat(page_back);
+  sendRequest('GET', url, null, token).then(function (res) {
+    check_coins();
+    var pay_link = document.querySelector('#pay-link');
+    pay_link.href = res.link;
+    pay_link.click();
+  })["catch"](function (err) {
+    return console.log(err);
+  });
+}
+
+var pay_btn = document.querySelector('.pay-btn');
+
+if (pay_btn) {
+  var points = document.querySelector('.points');
+  var inps = points.querySelectorAll('input[type="radio"]');
+
+  pay_btn.onclick = function (e) {
+    e.preventDefault();
+    inps.forEach(function (item, i) {
+      if (item.checked) GET_pay(item.value);
+    });
+  };
+};"use strict";
+
+function GET_userInfo() {
+  var url = "".concat(baseURL, "/user/info");
+  sendRequest('GET', url, null, token).then(function (res) {
+    ls.setItem('ai_nude_coins', "".concat(res.userWallet));
+    document.querySelector('.modal__overlay').click();
+    auth_select();
+    coins_update(res.userWallet);
+  })["catch"](function (err) {
+    return console.log(err);
+  });
+}
+
+if (token && username) {
+  GET_userInfo();
+};"use strict";
+
+function ls_data_user(_username, _token, _coins) {
+  ls.setItem('ai_nude_name', _username);
+  ls.setItem('ai_nude_token', _token);
+  ls.setItem('ai_nude_coins', _coins);
+};"use strict";
+
+function POST_auth(body) {
+  start_loader();
+  var url = "".concat(baseURL, "/user/auth");
+  sendRequest('POST', url, body).then(function (res) {
+    document.querySelector('.modal__overlay').click();
+    ls_data_user(res.userName, res.authToken, res.wallet);
+    location.reload();
+    remove_loader();
+  })["catch"](function (err) {
+    console.log(err);
+    remove_loader();
+  });
+};"use strict";
+
+function POST_image() {
+  var url = "".concat(baseURL, "/images/new");
+  var body = {
+    imageBase64: null
+  };
+
+  if (img_base_64) {
+    body.imageBase64 = img_base_64[0];
+  }
+
+  if (!body.imageBase64) {
+    modal('<h1>No image selected(</h1>');
+  }
+
+  if (body.imageBase64) {
+    start_loader();
+    sendRequest('POST', url, body, token).then(function (res) {
+      remove_loader();
+      window.location.pathname = routes.myPhotos;
+      GET_userInfo();
+    })["catch"](function (err) {
+      remove_loader();
+
+      if (err.code == 2) {
+        window.location.pathname = routes.deposit;
+      }
+
+      console.log(err);
+    });
+  }
+};"use strict";
+
+function POST_registration(body) {
+  start_loader();
+  var click_id = CUT_clickID(); // файл param_click_id
+
+  var url = "".concat(baseURL, "/user/registration");
+  body.clickId = click_id;
+  sendRequest('POST', url, body).then(function (res) {
+    document.querySelector('.modal__overlay').click();
+    ls_data_user(res.userName, res.authToken, res.wallet);
+    location.reload();
+    remove_loader();
+    REMOVE_clickID();
+  })["catch"](function (err) {
+    console.log(err);
+    remove_loader();
   });
 };"use strict";
 
@@ -442,7 +582,7 @@ function coins_update(value) {
 
 function send_image() {
   var form = document.querySelector('.create-form');
-  var login = document.querySelector('#sign_in');
+  var login = document.querySelector('#sign_up');
 
   if (form) {
     form.addEventListener('submit', function (e) {
@@ -530,6 +670,31 @@ function f_tabs() {
 
 f_tabs();;"use strict";
 
+if (window.location.pathname == routes.create) {
+  var add = function add(src) {
+    img_base_64 = [src];
+    block.insertAdjacentHTML('afterbegin', "<div class=\"create-form__preview\">\n\t\t\t<div class=\"createImg-remove-btn\">\xD7</div>\n\t\t\t<img src=\"data:image/png;base64,".concat(src, "\"/>\n\t\t</div>"));
+  };
+
+  var inp_img = document.querySelector('#create-form_img');
+  var block = document.querySelector('.create-form__block');
+  var img_save = ls.getItem('nude_img-file');
+
+  if (img_save) {
+    add(img_save);
+    var remove_btn = document.querySelector('.createImg-remove-btn');
+    remove_btn.addEventListener('click', function () {
+      var previews = document.querySelectorAll('.create-form__preview');
+      ls.removeItem('nude_img-file');
+      previews.forEach(function (el) {
+        return el.remove();
+      });
+      img_base_64 = null;
+      inp_img.value = '';
+    });
+  }
+}
+
 function update_img() {
   var inp_img = document.querySelector('#create-form_img');
   var block = document.querySelector('.create-form__block');
@@ -551,6 +716,8 @@ function getBase64(file) {
   reader.onload = function () {
     img_base_64 = reader.result;
     img_base_64 = [img_base_64.split(",")[1]];
+    ls.removeItem('nude_img-file');
+    ls.setItem('nude_img-file', img_base_64);
   };
 
   reader.onerror = function (error) {
@@ -574,138 +741,10 @@ function img_preview(file, img, block) {
   function remove() {
     var remove_btn = document.querySelector('.createImg-remove-btn');
     remove_btn.addEventListener('click', function () {
+      ls.removeItem('nude_img-file');
       remove_btn.parentNode.remove();
       img_base_64 = null;
       file.value = '';
     });
   }
-};"use strict";
-
-function GET_images() {
-  var url = "".concat(baseURL, "/images/list");
-  sendRequest('GET', url, null, token).then(function (res) {
-    addImagesToCollection(res);
-    modal_image();
-  })["catch"](function (err) {
-    return console.log(err);
-  });
-}
-
-var myPhotos = document.querySelector('.s_my-photos');
-if (myPhotos) GET_images();;"use strict";
-
-function GET_imgStatus(image_id) {
-  var url = "".concat(baseURL, "/images/check?image_id=").concat(image_id);
-  var check_image_status = setInterval(function () {
-    sendRequest('GET', url, null, token).then(function (res) {
-      if (res.state != 0) {
-        if (myPhotos) GET_images();
-        clearInterval(check_image_status);
-      }
-    })["catch"](function (err) {
-      return console.log(err);
-    });
-  }, 10000);
-};"use strict";
-
-function GET_pay(cash) {
-  var page_back = "".concat(window.location.origin).concat(routes.payinfo);
-  var url = "".concat(baseURL, "/payments/create?payment_type=1&payment_amount=").concat(cash, "&successfull=").concat(page_back);
-  sendRequest('GET', url, null, token).then(function (res) {
-    check_coins();
-    var pay_link = document.querySelector('#pay-link');
-    pay_link.href = res.link;
-    pay_link.click();
-  })["catch"](function (err) {
-    return console.log(err);
-  });
-}
-
-var pay_btn = document.querySelector('.pay-btn');
-
-if (pay_btn) {
-  var points = document.querySelector('.points');
-  var inps = points.querySelectorAll('input[type="radio"]');
-
-  pay_btn.onclick = function (e) {
-    e.preventDefault();
-    inps.forEach(function (item, i) {
-      if (item.checked) GET_pay(item.value);
-    });
-  };
-};"use strict";
-
-function GET_userInfo() {
-  var url = "".concat(baseURL, "/user/info");
-  sendRequest('GET', url, null, token).then(function (res) {
-    ls.setItem('ai_nude_coins', "".concat(res.userWallet));
-    document.querySelector('.modal__overlay').click();
-    auth_select();
-    coins_update(res.userWallet);
-  })["catch"](function (err) {
-    return console.log(err);
-  });
-}
-
-if (token && username) {
-  GET_userInfo();
-};"use strict";
-
-function ls_data_user(_username, _token, _coins) {
-  ls.setItem('ai_nude_name', _username);
-  ls.setItem('ai_nude_token', _token);
-  ls.setItem('ai_nude_coins', _coins);
-};"use strict";
-
-function POST_auth(body) {
-  start_loader();
-  var url = "".concat(baseURL, "/user/auth");
-  sendRequest('POST', url, body).then(function (res) {
-    document.querySelector('.modal__overlay').click();
-    ls_data_user(res.userName, res.authToken, res.wallet);
-    location.reload();
-    remove_loader();
-  })["catch"](function (err) {
-    console.log(err);
-    remove_loader();
-  });
-};"use strict";
-
-function POST_image() {
-  start_loader();
-  var url = "".concat(baseURL, "/images/new");
-  var body = {
-    imageBase64: img_base_64[0]
-  };
-  sendRequest('POST', url, body, token).then(function (res) {
-    remove_loader();
-    window.location.pathname = routes.myPhotos;
-    GET_userInfo();
-  })["catch"](function (err) {
-    remove_loader();
-
-    if (err.code == 2) {
-      window.location.pathname = routes.deposit;
-    }
-
-    console.log(err);
-  });
-};"use strict";
-
-function POST_registration(body) {
-  start_loader();
-  var click_id = CUT_clickID(); // файл param_click_id
-
-  var url = "".concat(baseURL, "/user/registration");
-  body.clickId = click_id;
-  sendRequest('POST', url, body).then(function (res) {
-    document.querySelector('.modal__overlay').click();
-    ls_data_user(res.userName, res.authToken, res.wallet);
-    location.reload();
-    remove_loader();
-    REMOVE_clickID();
-  })["catch"](function (err) {
-    console.log(err);
-    remove_loader();
-  });
 }
